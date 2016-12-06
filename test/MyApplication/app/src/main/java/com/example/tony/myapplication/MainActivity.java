3cf8b5a13cf3cce954e1ba707f0a4bb3018286d9
zhaowenhao <zhaowenhao@chinamobile.com>
2016-12-06 16:03:15 +0800

    zhao

--- test/MyApplication/app/src/main/java/com/example/tony/myapplication/MainActivity.java
+++ test/MyApplication/app/src/main/java/com/example/tony/myapplication/MainActivity.java
@@ -6,8 +6,6 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
